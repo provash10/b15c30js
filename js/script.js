055bfe3900001b7330997bc23e23a879a42cb344
provash10 <provash20cb@gmail.com>
2024-10-21 22:41:36 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,80 @@
+// Loop
+// Loop types
+// 1. for Loop
+// 2. foreach loop
+// 3. while loop
+// 4. do while loop
+
+// console.log("Hello World");
+
+// 1. for Loop structure
+
+// for (initialization; range/condition; increment/decriment)
+//       1                    10             ++
+//     10                      5             --
+
+// for (initialization; range/condition; increment/decriment){
+//     //statement................
+// }
+
+// for (let i=1; i<=10;  i++){ //1, 2, 3, 4 .......9, 10 11
+//     console.log("Hello World");
+// }
+
+// for (let i=1; i<=10;  i++){ //1, 2, 3, 4 .......9, 10, 11
+//     console.log("Hello World");
+//     console.log(i);
+// }
+
+// let marks = [23, 67, 89, 99, 33];
+
+
+// marks[0];
+// marks[1];
+// marks[2];
+// marks[3];
+// marks[4];
+
+// console.log(marks[0]);
+// console.log(marks[1]);
+// console.log(marks[2]);
+// console.log(marks[3]);
+// console.log(marks[4]);
+
+//  for (let i=0; i<=4; i++){  // 0, 1, 2, 3, 4, 5
+//     // console.log(marks[0]);
+//     console.log(marks[i]);
+//  }
+
+let sorry = [50];
+
+for(let i=0; i<=50; i++){ 
+    // console.log(sorry[0]);
+    console.log(sorry[i]);
+}
+
+
+// ==================================
+
+// let number = 20;
+
+// if(number%2 == 0){
+//     console.log("Even Number");
+// }
+
+// else {
+//     console.log("Even Number");
+// }
+
+// let numbers = [34, 56, 67, 90, 45, 74,90, 123, 76,100];
+
+// for(let i=0; i<=9; i++){ // 0, 1, 2, 3,......9,10
+//     if(numbers[i] % 2 == 0){
+//         console.log("Even Number=" + numbers[i]);
+//     }
+    
+//     else {
+//         console.log("Odd Number=" + numbers[i]);
+//     }
+// }
+
